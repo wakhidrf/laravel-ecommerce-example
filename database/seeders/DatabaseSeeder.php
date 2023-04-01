f474d52e7449aa82685c9aff0800f47c53dcf605
Wakhid Rahmad Faisol <wakhidrf@Ferrea>
2023-04-01 07:22:26 +0700

    Update 0.0.1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,7 +6,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
